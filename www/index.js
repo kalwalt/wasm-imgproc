840fbe787607cce323bf93e16ff780bd63b09c5a
kalwalt <github@kalwaltart.it>
2021-03-14 01:01:47 +0100

    adding www folder

---
+++ www/index.js
@@ -0,0 +1,12 @@
+import * as wasm from "wasm_imgproc";
+
+console.log(wasm);
+let img = wasm.image_create(100, 100);
+console.log(img);
+let canvas = document.getElementById('wasmCanvas');
+let ctx = canvas.getContext('2d');
+let imageData = ctx.createImageData(100,100)
+console.log(imageData);
+imageData.data.set(img);
+ctx.putImageData(imageData, 100, 100)
+
